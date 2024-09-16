9dab7ed47e9618fd510c8c6fedb6bc0047f9d04c
Emmanuel <asamoahe304@gmail.com>
2024-09-16 07:57:20 +0000

    updated icon packs

---
+++ src/icons/WalletTick.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import createPerkIcon from "@/utils/createPerkIcon";
+import { IconWrapperProps } from "@/utils/createPerkIcon";
+
+//@ts-ignore
+const WalletTick: React.FC<IconWrapperProps> = ({ color = 'currentColor', set = 'outline', strokeWidth }) => {
+    const Bold = () => (
+        <g></g>
+    )
+
+    const Broken = () => (
+        <g></g>
+    )
+
+    const Bulk = () => (
+        <g></g>
+    )
+    const Linear = () => (
+        <g></g>
+    )
+    const Outline = () => (
+        <g></g>
+    )
+    const TwoTone = () => (
+        <g></g>
+    )
+
+    const iconMap = {
+        bold: <Bold />,
+        broken: <Broken />,
+        bulk: <Bulk />,
+        linear: <Linear />,
+        outline: <Outline />,
+        twoTone: <TwoTone />,
+    };
+
+    return iconMap[set] || <Outline />;
+}
+
+WalletTick.displayName = "WalletTick"
+export default createPerkIcon(WalletTick)
